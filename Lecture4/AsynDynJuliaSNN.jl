6c1d71bd5245a7a62ec52db49dcb4c042b2a70e4
Alessio <alessio.quaresima@mpi.nl>
2025-09-16 22:44:24 +0200

    Added Zerlaut code

--- Lecture4/AsynDynJuliaSNN.jl
+++ Lecture4/AsynDynJuliaSNN.jl
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -25,6 +26,73 @@ using Plots
 
 global_logger(ConsoleLogger())
 SNN.@load_units
+
+# %%
+import SpikingNeuralNetworks: IF, IFSinExpParameter, PoissonLayerParameter, PoissonLayer, SpikingSynapse
+
+Zerlaut2019_network = (Npop = (E=4000, I=1000),
+    exc = IFSinExpParameter(
+                τm = 200pF / 10nS, 
+                El = -70mV, 
+                Vt = -50.0mV, 
+                Vr = -70.0f0mV,
+                R  = 1/10nS, 
+                τabs = 2ms,       
+                τi=5ms,
+                τe=5ms,
+                E_i = -80mV,
+                E_e = 0mV,
+                ),
+
+    inh = IFSinExpParameter(
+                τm = 200pF / 10nS, 
+                El = -70mV, 
+                Vt = -53.0mV, 
+                Vr = -70.0f0mV,
+                R  = 1/10nS, 
+                τabs = 2ms,       
+                τi=5ms,
+                τe=5ms,
+                E_i = -80mV,
+                E_e = 0mV,
+                ),
+
+    connections = (
+        E_to_E = (p = 0.05, μ = 2nS),
+        E_to_I = (p = 0.05, μ = 2nS),
+        I_to_E = (p = 0.05, μ = 10nS),
+        I_to_I = (p = 0.05, μ = 10nS),
+        ),
+    
+    afferents = (
+        N = 100,
+        p = 0.1f0,
+        rate = 20Hz,
+        μ = 4.0,
+        ), 
+)
+
+function network(config)
+    @unpack afferents, connections, Npop = config
+    E = IF(N=Npop.E, param=config.exc, name="E")
+    I = IF(N=Npop.I, param=config.inh, name="I")
+
+    AfferentParam = PoissonLayerParameter(afferents.rate; afferents...)
+    afferentE = PoissonLayer(E, :ge, param=AfferentParam, name="noiseE")
+    afferentI = PoissonLayer(I, :ge, param=AfferentParam, name="noiseI")
+
+    synapses = (
+        E_to_E = SpikingSynapse(E, E, :ge, p=connections.E_to_E.p, μ=connections.E_to_E.μ, name="E_to_E"),
+        E_to_I = SpikingSynapse(E, I, :ge, p=connections.E_to_I.p, μ=connections.E_to_I.μ, name="E_to_I"),
+        I_to_E = SpikingSynapse(I, E, :gi, p=connections.I_to_E.p, μ=connections.I_to_E.μ, name="I_to_E"),
+        I_to_I = SpikingSynapse(I, I, :gi, p=connections.I_to_I.p, μ=connections.I_to_I.μ, name="I_to_I"),
+    )
+    model = SNN.compose(;E,I, afferentE, afferentI, synapses..., silent=true, name="Balanced network") 
+    SNN.monitor!(model.pop, [:fire])
+    SNN.monitor!(model.stim, [:fire])
+    # monitor!(model.pop, [:v], sr=200Hz)
+    return SNN.compose(;model..., silent=true)
+end
 
 # %%
 model = network(Zerlaut2019_network) 
